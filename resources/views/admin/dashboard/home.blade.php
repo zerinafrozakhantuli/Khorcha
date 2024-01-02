3e151244af2bae4be6a551dc4ffd021213fdd771
Zerin Afroza Khan <zerinafroza.tuli@gmail.com>
2024-01-01 20:53:20 -0800

    Pie chart add

--- resources/views/admin/dashboard/home.blade.php
+++ resources/views/admin/dashboard/home.blade.php
@@ -26,6 +26,26 @@
               </div>  
             </div>
         </div>
+
+        <div class="col-md-6">
+            <div class="card mb-3">
+              <div class="card-header">
+                <div class="row">
+                    <div class="col-md-12 card_title_part">
+                        <i class="fab fa-gg-circle"></i>All Expense Category Information
+                    </div>  
+                      
+                </div>
+              </div>
+              <div class="card-body" style="margin: 0px auto;">
+
+               <canvas id="myPieChart"></canvas>
+              </div>
+              <div class="card-footer">
+                
+              </div>  
+            </div>
+        </div>
     </div>
     @php
     $allCate=App\Models\IncomeCategory::where('incate_status',1)->orderBy('incate_id','ASC')->get();
@@ -45,8 +65,16 @@
       @endphp
       ],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Income Source',
+        data: [
+        @php
+        foreach( $allCate as $cate){
+            $cateID=$cate->incate_id;
+            $cate_income=App\Models\Income::where('income_status',1)->where('incate_id',$cateID)->sum('income_amount');
+          echo $cate_income.',';
+            }
+        @endphp
+        ],
         borderWidth: 1
       }]
     },
@@ -60,7 +88,40 @@
   });
 </script>
 
+<!-- Pie chart start -->
+<script>
+ const ctx_pie = document.getElementById('myPieChart');  
+
+  new Chart(ctx_pie, {
+    type: 'pie',
+    data: {
+      labels: 
+      [
+      @php
+      foreach( $allCate as $cate){
+      echo "'".$cate->incate_name ."',";
+     }
+      @endphp
+      ],
+      datasets: [{
+        label: 'Income Source',
+        data: [
+        @php
+        foreach( $allCate as $cate){
+            $cateID=$cate->incate_id;
+            $cate_income=App\Models\Income::where('income_status',1)->where('incate_id',$cateID)->sum('income_amount');
+          echo $cate_income.',';
+            }
+        @endphp
+        ],
+         hoverOffset: 4
+      }]
+    },
     
+  });
+</script>
+
+
 
 @endsection
 
